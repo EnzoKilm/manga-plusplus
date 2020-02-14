998ed3659cf8a421417ef17f530ad651325b5f88
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-14 12:01:11 +0100

    Ajout erreur 404

---
+++ template-site/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php'; ?>
+	
+	<section id="erreur_404">
+		<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="index.php">Manga++</a></li>
+				  <li class="active">404</li>
+				</ol>
+			</div>
+			<div class="row">
+				<div class="container-fluid">
+					<!-- 404 Error Text -->
+					<div class="text-center">
+						<div class="error mx-auto" data-text="404">404</div>
+						<p class="lead text-gray-800 mb-5">Page non trouvée</p>
+					</div>
+				</div>
+				<!-- /.container-fluid -->
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+	
+	<?php include 'footer.php'; ?>
+</body>
+</html>
