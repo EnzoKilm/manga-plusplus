3e077941485af133ca5ed0c8fb42b60578a240c5
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-14 01:03:15 +0100

    Ajout page de login

--- template-site/profile.php
+++ template-site/profile.php
@@ -2,17 +2,20 @@
 <html lang="en">
 <?php include 'header.php'; ?>
 	
-	<section id="profile_content">
+    <section>
 		<div class="container">
 			<div class="breadcrumbs">
 				<ol class="breadcrumb">
 				  <li><a href="index.php">Manga++</a></li>
 				  <li class="active">Profil</li>
 				</ol>
-            </div>
-            <div>
-
-            </div>
+			</div>
+		</div>
+    </section>
+    
+	<section id="profile_content">
+		<div class="container">
+            
 		</div>
 	</section> <!--/#profile_content-->
 	
