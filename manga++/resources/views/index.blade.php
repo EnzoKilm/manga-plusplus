4c29e20f1d4f2782ba8468d58c3871edbf55143d
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-17 23:28:18 +0100

    Tri page index et liens recherche mangas/bd

--- manga++/resources/views/index.blade.php
+++ manga++/resources/views/index.blade.php
@@ -60,75 +60,27 @@
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-                                                <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                                Mangas
-                                            </a>
+                                            <form method="post" action="{{ route('public.search') }}">
+                                                @csrf
+                                                <input type="hidden" name="search" placeholder="Rechercher" value="Manga"/>
+                                                <button type="submit" class="index-button"><a>Mangas</a></button>
+                                            </form>
                                         </h4>
-                                    </div>
-                                    <div id="sportswear" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <ul>
-                                                <li><a href="#">One Piece </a></li>
-                                                <li><a href="#">Dragon Ball </a></li>
-                                                <li><a href="#">Death Note </a></li>
-                                                <li><a href="#">Fullmetal Alchemist</a></li>
-                                                <li><a href="#">Berserk </a></li>
-                                            </ul>
-                                        </div>
                                     </div>
                                 </div>
                                 <div class="panel panel-default">
                                     <div class="panel-heading">
                                         <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordian" href="#mens">
-                                                <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                                Bandes dessinées
-                                            </a>
+                                            <form method="post" action="{{ route('public.search') }}">
+                                                @csrf
+                                                <input type="hidden" name="search" placeholder="Rechercher" value="Bande dessinée"/>
+                                                <button type="submit" class="index-button"><a>Bandes dessinées</a></button>
+                                            </form>
                                         </h4>
-                                    </div>
-                                    <div id="mens" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <ul>
-                                                <li><a href="#">Watchmen</a></li>
-                                                <li><a href="#">Calvin et Hobbes</a></li>
-                                                <li><a href="#">Walking Dead</a></li>
-                                                <li><a href="#">V pour Vendetta</a></li>
-                                                <li><a href="#">La Jeunesse de Picsou</a></li>
-                                                <li><a href="#">Gaston</a></li>
-                                                <li><a href="#">Killing Joke</a></li>
-                                                <li><a href="#">Astérix et Obélix</a></li>
-                                            </ul>
-                                        </div>
                                     </div>
                                 </div>
 
                             </div><!--/category-products-->
-
-                            <div class="brands_products"><!--brands_products-->
-                                <h2>Exemplaires</h2>
-                                <div class="brands-name">
-                                    <ul class="nav nav-pills nav-stacked">
-                                        <li><a href="#"> <span class="pull-right">(50)</span>Astérix et Obélix</a></li>
-                                        <li><a href="#"> <span class="pull-right">(56)</span>Naruto</a></li>
-                                        <li><a href="#"> <span class="pull-right">(27)</span>Boule et Bill</a></li>
-                                        <li><a href="#"> <span class="pull-right">(32)</span>Hunter X Hunter</a></li>
-                                        <li><a href="#"> <span class="pull-right">(5)</span>Titeuf</a></li>
-                                    </ul>
-                                </div>
-                            </div><!--/brands_products-->
-
-                            <div class="price-range"><!--price-range-->
-                                <h2>Tranche de prix</h2>
-                                <div class="well text-center">
-                                     <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="100" data-slider-step="1" data-slider-value="[0,20]" id="sl2" ><br />
-                                     <b class="pull-left">0€</b> <b class="pull-right">100€</b>
-                                </div>
-                            </div><!--/price-range-->
-
-                            <!--<div class="shipping text-center">
-                                <img src="public_site/images/home/shipping.jpg" alt="" />
-                            </div>-->
 
                         </div>
                     </div>
@@ -228,103 +180,6 @@
                                 </div>
                             </div>
                         </div><!--/category-tab-->
-
-                        <div class="recommended_items"><!--recommended_items-->
-                            <h2 class="title text-center">Articles recommandés</h2>
-
-                            <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="item active">
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/naruto_cover.png" alt="Naruto" />
-                                                        <h2>56€</h2>
-                                                        <p>Naruto, Tome 63</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/titeuf_cover.jpg" alt="Titeuf" />
-                                                        <h2>56€</h2>
-                                                        <p>Titeuf, Tome 3 : Ça épate les filles</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/naruto_cover.png" alt="Naruto" />
-                                                        <h2>56€</h2>
-                                                        <p>Naruto, Tome 63</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/titeuf_cover.jpg" alt="Titeuf" />
-                                                        <h2>56€</h2>
-                                                        <p>Titeuf, Tome 3 : Ça épate les filles</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/naruto_cover.png" alt="Naruto" />
-                                                        <h2>56€</h2>
-                                                        <p>Naruto, Tome 63</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="product-image-wrapper">
-                                                <div class="single-products">
-                                                    <div class="productinfo text-center">
-                                                        <img src="public_site/images/home/titeuf_cover.jpg" alt="Titeuf" />
-                                                        <h2>56€</h2>
-                                                        <p>Titeuf, Tome 3 : Ça épate les filles</p>
-                                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Ajouter au panier</a>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
-                                    <i class="fa fa-angle-left"></i>
-                                  </a>
-                                  <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
-                                    <i class="fa fa-angle-right"></i>
-                                  </a>
-                            </div>
-                        </div><!--/recommended_items-->
 
                     </div>
                 </div>
