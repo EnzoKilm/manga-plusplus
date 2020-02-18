c039e58ced4152e6b7cf0fd70c8b7abf939217e9
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-18 18:42:26 +0100

    Modif header/suppr img

--- manga++/resources/views/admin/footer.blade.php
+++ manga++/resources/views/admin/footer.blade.php
@@ -20,7 +20,7 @@
   </a>
 
   <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  {{-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -36,7 +36,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
 
   <!-- Bootstrap core JavaScript-->
   <script src="{{ URL::to('/') }}/admin_site/vendor/jquery/jquery.min.js"></script>
