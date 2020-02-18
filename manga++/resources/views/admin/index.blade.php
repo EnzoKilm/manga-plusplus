daf4706f75441eb5ab7ff61a47f34bfb5c918154
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-18 23:29:16 +0100

    Nettoyage panel admin

--- manga++/resources/views/admin/index.blade.php
+++ manga++/resources/views/admin/index.blade.php
@@ -132,7 +132,7 @@
 
           <!-- Content Row -->
 
-          <div class="row">
+          {{-- <div class="row">
 
             <!-- Area Chart -->
             <div class="col-xl-8 col-lg-7">
@@ -291,24 +291,10 @@
 
             </div>
 
-            <div class="col-lg-6 mb-4">
-
-              <!-- Illustrations -->
-              <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                </div>
-                <div class="card-body">
-                  <div class="text-center">
-                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
-                  </div>
-                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-                </div>
-              </div>
+            <div class="col-lg-6 mb-4"> --}}
 
               <!-- Approach -->
-              <div class="card shadow mb-4">
+              {{-- <div class="card shadow mb-4">
                 <div class="card-header py-3">
                   <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
                 </div>
@@ -319,7 +305,17 @@
               </div>
 
             </div>
-          </div>
+          </div> --}}
+
+        <!-- Illustrations -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Panel d'administration</h6>
+            </div>
+            <div class="card-body">
+                <p>Bienvenue sur le panel d'administration de votre site.</p>
+            </div>
+        </div>
 
         </div>
         <!-- /.container-fluid -->
